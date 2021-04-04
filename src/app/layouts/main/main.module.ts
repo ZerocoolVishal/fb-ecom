4e7aa786db409a682f44e072770a1f6226a39b5b
Vishal Bhosle <vishalbhosle83@gmail.com>
2021-04-04 12:52:42 +0530

    E-commerce theme implemented

--- src/app/layouts/main/main.module.ts
+++ src/app/layouts/main/main.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main.component';
+import {RouterModule} from '@angular/router';
 
 
 
@@ -9,8 +10,9 @@ import { MainComponent } from './main.component';
     exports: [
         MainComponent
     ],
-    imports: [
-        CommonModule
-    ]
+  imports: [
+    CommonModule,
+    RouterModule
+  ]
 })
 export class MainModule { }
