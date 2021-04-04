b3611ffc492b6ed8d655ddedc79cd7df5d35a5b7
Vishal Bhosle <vishalbhosle83@gmail.com>
2021-04-04 19:49:21 +0530

    product details page updated

--- src/app/pages/product-details/product-details.component.ts
+++ src/app/pages/product-details/product-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Product} from '../../models/product';
 
 @Component({
   selector: 'app-product-details',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
+  product: Product;
+  quantity = 1;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.product = {
+      id: '123456',
+      name: 'White Modern Chair',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid quae eveniet culpa officia quidem mollitia impedit iste asperiores nisi reprehenderit consequatur, autem, nostrum pariatur enim?',
+      price: 2300,
+      quantity: 10
+    };
   }
 
 }
--- src/app/pages/product-details/product-details.module.ts
+++ src/app/pages/product-details/product-details.module.ts
@@ -3,13 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { ProductDetailsRoutingModule } from './product-details-routing.module';
 import { ProductDetailsComponent } from './product-details.component';
+import {MobileNavModule} from '../../layouts/mobile-nav/mobile-nav.module';
+import {HeaderModule} from '../../layouts/header/header.module';
+import {FormsModule} from '@angular/forms';
 
 
 @NgModule({
   declarations: [ProductDetailsComponent],
   imports: [
     CommonModule,
-    ProductDetailsRoutingModule
+    ProductDetailsRoutingModule,
+    MobileNavModule,
+    HeaderModule,
+    FormsModule
   ]
 })
 export class ProductDetailsModule { }
