8f9aca98dd43ed32f0654f189375a5c92df2fdf9
Vishal Bhosle <vishalbhosle83@gmail.com>
2021-04-04 20:00:25 +0530

    shop updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCgTITo9HSb3L1TSDpE4FnLXLT0NeIayMI',
+    authDomain: 'fire-ecom.firebaseapp.com',
+    projectId: 'fire-ecom',
+    storageBucket: 'fire-ecom.appspot.com',
+    messagingSenderId: '929231096002',
+    appId: '1:929231096002:web:f496f9c3f204c97208935b',
+    measurementId: 'G-22DFTHDWVJ'
+  }
 };
