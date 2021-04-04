5ea0b2aa4a4b1f88c65dee3a47e76262589efdd8
Vishal Bhosle <vishalbhosle83@gmail.com>
2021-04-04 19:15:26 +0530

    shop updated and shop made default page

--- src/app/pages/shop/shop.component.ts
+++ src/app/pages/shop/shop.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShopComponent implements OnInit {
 
+  products: any[] = [1, 2, 3, 4, 5, 6];
+
   constructor() { }
 
   ngOnInit(): void {
